39583e965cc0622a13ae72e913734fa54f5d7cae
Dev <devmn204@gmail.com>
2026-04-02 22:28:03 +0530

    Update server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+app.use(express.static("public"));
+
+let players = {};
+
+io.on("connection", (socket) => {
+  console.log("Player connected:", socket.id);
+
+  players[socket.id] = {
+    x: 50,
+    y: Math.random() * 400 + 50
+  };
+
+  socket.on("move", (data) => {
+    let player = players[socket.id];
+    if (!player) return;
+
+    // Simple movement validation (anti-cheat)
+    const speed = 5;
+
+    if (data === "RIGHT") player.x += speed;
+    if (data === "LEFT") player.x -= speed;
+    if (data === "UP") player.y -= speed;
+    if (data === "DOWN") player.y += speed;
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    console.log("Player disconnected:", socket.id);
+  });
+});
+
+// Send updates 60 times/sec
+setInterval(() => {
+  io.sockets.emit("state", players);
+}, 1000 / 60);
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
